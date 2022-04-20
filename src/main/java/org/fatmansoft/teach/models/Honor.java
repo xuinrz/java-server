399be29933a2cdf936674ac413f7461d4ab0b8c4
xuinrz <2806229200@qq.com>
2022-04-20 21:42:30 +0800

    honor页面(许子璇)

---
+++ src/main/java/org/fatmansoft/teach/models/Honor.java
@@ -0,0 +1,64 @@
+package org.fatmansoft.teach.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(	name = "honor",
+        uniqueConstraints = {
+        })
+public class Honor {
+    @Id
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "StudentId")
+    private Student student;
+
+    @Size(max = 2)
+    private String honorLevel;
+    private String honorName;
+    private Date honorDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String getHonorLevel() {
+        return honorLevel;
+    }
+
+    public void setHonorLevel(String honorLevel) {
+        this.honorLevel = honorLevel;
+    }
+
+    public String getHonorName() {
+        return honorName;
+    }
+
+    public void setHonorName(String honorName) {
+        this.honorName = honorName;
+    }
+
+    public Date getHonorDate() {
+        return honorDate;
+    }
+
+    public void setHonorDate(Date honorDate) {
+        this.honorDate = honorDate;
+    }
+}
