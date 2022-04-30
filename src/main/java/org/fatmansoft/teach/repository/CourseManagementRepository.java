31061c4a736cb654a818f9ed8fe91e87fa48266b
xuinrz <2806229200@qq.com>
2022-04-30 23:14:59 +0800

    CourseManagementRepository修改Bug

--- src/main/java/org/fatmansoft/teach/repository/CourseManagementRepository.java
+++ src/main/java/org/fatmansoft/teach/repository/CourseManagementRepository.java
@@ -9,11 +9,11 @@ import java.util.List;
 @Repository
 public interface CourseManagementRepository extends JpaRepository<CourseManagement,Integer>
 {
-    @Query(value = "select max(id) from Daily ")
+    @Query(value = "select max(id) from CourseManagement ")
     Integer getMaxId();
     List<CourseManagement> findByStudentId(Integer studentId);
     //List<Daily> findByCourseCourseNum(String courseNum);
-    @Query(value = " from Daily d where (?1='' or (d.student.studentNum like %?1%) or (d.student.studentName like %?1%)) and (d.category like %?2% or ?2='')")
+    @Query(value = " from Log d where (?1='' or (d.student.studentNum like %?1%) or (d.student.studentName like %?1%)) and (d.category like %?2% or ?2='')")
     List<CourseManagement> findByNumNameType(String numName,String type);
 
     @Query(value = " from CourseManagement s where (?1='' or (s.student.studentNum like %?1%) or (s.student.studentName like %?1%)) and (s.course.courseName like concat('%',?2,'%') or ?2='') ")
